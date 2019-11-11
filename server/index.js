c707cc21997ed264c7c2384137828cb3697bc9f6
robertramosastudillo <robertramosastudillo@gmail.com>
2019-11-11 16:30:28 -0500

    Socket connection

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ let app = express();
 let server = require('http').Server(app);
 let io = require('socket.io')(server);
 
+app.use(express.static('client'));
+
 app.get('/hola-mundo', (req, res) => {
     res.status(200).send('Hola mundo desde una ruta');
+});
+
+io.on('connection', (socket) => {
+    console.log('El cliente con IP: ' + socket.handshake.address + 'se ha conectado...');
 });
 
 server.listen(6677, () => {
